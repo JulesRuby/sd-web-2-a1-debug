94b4bc0a1e9050b6262062f208858b2c1c8cdd64
JulesRuby <JulesRuby@users.noreply.github.com>
2025-09-04 21:42:49 -0600

    Added in excersize part 2. Filter by age and output.

--- index.js
+++ index.js
@@ -89,6 +89,10 @@ populateList(namesList, users);
 
 // 2. Print out the names of characters whose age is less than 40 in the console, then render them in the HTML list with id "young-characters-list"
 
+const ageFortyArray = users.filter((user) => user.age < 40);
+logUserData(ageFortyArray);
+populateList(youngCharactersList, ageFortyArray); // TODO: adjust to allow choice of which attributes to place in list items
+
 // 3. Create a reusable function that takes any array and uses logic to render a list of character names in the HTML. Use this function to populate the list with id "function-list"
 
 // 4. Create a function that takes an array and an age threshold parameter. The function should only display characters whose age is below the given number. Render results in the list with id "age-filter-list"
